43bba6db1c9f1a6474d7cf5c007f5068e3d50f31
Chris Borchert <cborchertrun@gmail.com>
2018-09-04 19:58:20 +0200

    Set up skeleton

---
+++ redux/reducers/index.test.js
@@ -0,0 +1,12 @@
+import reducer from './index';
+
+//Borrowing heavily from https://medium.com/@netxm/testing-redux-reducers-with-jest-6653abbfe3e1
+//and the docs https://redux.js.org/recipes/writingtests#reducers
+describe('reducer', ()=>{
+    it('should return an initial state', ()=>{}); 
+    it('should handle SETUP_GAME', ()=>{}); 
+    it('should handle TOGGLE_PAUSE_GAME', ()=>{}); 
+    it('should handle SET_HOVERED_SLOT', ()=>{}); 
+    it('should handle SLOT_TILE', ()=>{}); 
+    it('should handle UNSLOT_TILE', ()=>{}); 
+});
